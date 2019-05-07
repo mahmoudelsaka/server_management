4a6de46b36ca930d91056f903671c1731d36f2a3
mahmoudelsaka <mshaban2010.ms@gmail.com>
2019-05-07 17:07:36 +0200

    Update __manifest__.py

--- server_management/__manifest__.py
+++ server_management/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Server Management',
-    'version': '11.0',
+    'version': '12.0',
     'category': 'Management',
     'description': """
 Managing Company's Servers:
 * Server Management Menu: Human Resources >> Server Management >> Server Management
 * Server Management Groups configurable from user profile: Server Access: Limited and Server Access: Full
     """,
-    'author': 'Rightechs Solution',
+    'author': 'Rightechs Solutions',
     'website': 'rightechs.net',
     'depends': ['base', 'hr'],
     'data': [
